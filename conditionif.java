7d9e49aa5c992bd918cdb3252461d5fec181acd7
amitshukla12345 <amitshukl22509@gmail.com>
2024-10-13 18:55:12 +0530

    first commmit

---
+++ conditionif.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class conditionif{
+	public static void main(String[] args){
+		int pwd;
+		System.out.print(" Enter password  ");
+		Scanner obj=new Scanner(System.in);
+		pwd = obj.nextInt();
+		if(pwd==2731)
+		{
+			System.out.println("my Name:=amit");
+			System.out.println("my age=21");
+			System.out.println("my cotact:=7738169470");
+			System.out.println("my address:= vile parle road no 5");
+		}
+	}
+}
