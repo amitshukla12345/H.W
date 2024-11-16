85d14e7006ae8ba33d336c954810d501e21574b7
amitshukla12345 <amitshukl22509@gmail.com>
2024-11-16 17:56:44 +0530

    first commit

---
+++ scop.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+
+    // Code here CANNOT use x
+
+    int x = 100;
+
+    // Code here can use x
+    System.out.println(x);
+  }
+}
+
